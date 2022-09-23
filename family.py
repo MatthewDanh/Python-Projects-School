96240e69d0ab9a50203a98fee0330df4be1ae7fa
Glitch <64738939+MatthewDanh@users.noreply.github.com>
2022-09-23 01:30:30 -0700

    Add files via upload

---
+++ family.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python2.7
+"""Blank Python 2.7 file"""
+family = {'MegAhurtz': 23, 'LottaRam': 21, 'DeeBug': 11, 'AuntieVirus': 56}
+print(family)
+print(family['LottaRam'])
+family["Tim"] = 23
+print(family)
+print(family[input('Enter a name: ')])
---
+++ name.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python2.7
+"""Blank Python 2.7 file"""
+first = input("Enter first name: ")
+last = input("Enter last name: ")
+fullName = first+" "+last
+print(fullName)
+print(len(fullName))
+print((first[0]+last[0]).upper())
+print(first[3])
